9d374e93c98836c5e52315f5b3f9d85e3a47f9ce
Abraham Owodunni <Abrahamowodunni@gmail.com>
2024-10-10 19:53:44 -0700

    dataset.py in progress

--- dataset.py
+++ dataset.py
@@ -28,8 +28,23 @@ class BillingualDataSet(Dataset):
         dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
 
         # Add sos, eos, and padding to each sentence
-        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
-        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 ## start and end of token here. 
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1 # only start of token
 
         if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
             raise ValueError("Sentence is too long")
+
+        # Add <s> and </s> tokens
+        encoder_input = torch.cat([
+            self.sos_token,
+            torch.tensor(enc_input_tokens, dtype=torch.int64),
+            self.eos_token,
+            torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64),
+        ], dim=0)
+
+        # Add only <s> token
+        decoder_input = torch.cat([
+            self.sos_token,
+            torch.tensor(dec_input_tokens, dtype=torch.int64),
+            torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64),
+        ], dim=0)
